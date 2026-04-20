0d763e771629de7a2d1ad4f3a739400253b4c321
Erbolsyn259 <y_omirzak@kbtu.kz>
2026-04-20 16:41:16 +0600

    Update serializers.py

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+
+class TokenResponseSerializer(serializers.Serializer):
+    token = serializers.CharField(read_only=True)
+    user_id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(read_only=True)
 
 class UserSerializer(serializers.ModelSerializer):
 
